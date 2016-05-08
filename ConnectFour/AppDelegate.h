90703746a70a7880350769e26b04f9faa4aeba13
aaronpankratz <pankratz.aaron@gmail.com>
2016-05-08 14:56:57 -0500

    Initialize project

---
+++ ConnectFour/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConnectFour
+//
+//  Created by Aaron Pankratz on 5/8/16.
+//  Copyright © 2016 Aaron Pankratz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConnectFour/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ConnectFour
+//
+//  Created by Aaron Pankratz on 5/8/16.
+//  Copyright © 2016 Aaron Pankratz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
